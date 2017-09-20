a6ebc93ea30a9ee485cedc561355b17226889239
Hawkon Gustavsson <h.gustavsson96@gmail.com>
2017-09-20 14:57:48 -0400

    Changed server.js to split the switch

--- src/server.js
+++ src/server.js
@@ -9,58 +9,49 @@ const handleGET = (request, response, parsedURL) => {
   let status;
   const accepted = request.headers.accept.split(',');
   const type = accepted[0] || 'application/json';
-  // console.dir(parsedURL);
   const params = query.parse(parsedURL.query);
-  switch (parsedURL.pathname) {
-    case '/style.css':
-      handler.getCSS(request, response);
-      break;
-    case '/client.html':
-    case '/':
-      handler.getIndex(request, response);
-      break;
-    case '/success':
-      status = 200;
-      handler.respond(request, response, status, type);
-      break;
-    case '/badRequest':
-      if (params.valid) {
+  if (parsedURL.pathname === '/style.css') {
+    handler.getCSS(request, response);
+  } else if (parsedURL.pathname === '/' || parsedURL.pathname === '/client.html') {
+    handler.getIndex(request, response);
+  } else {
+    switch (parsedURL.pathname) {
+      case '/success':
         status = 200;
-      } else {
-        status = 400;
-      }
-      handler.respond(request, response, status, type);
-      break;
-    case '/unauthorized':
-      if (params.loggedIn === 'yes') {
-        status = 200;
-      } else {
-        status = 401;
-      }
-      handler.respond(request, response, status, type);
-      break;
-    case '/forbidden':
-      status = 403;
-      handler.respond(request, response, status, type);
-      break;
-    case '/internal':
-      status = 500;
-      handler.respond(request, response, status, type);
-      break;
-    case '/notImplemented':
-      status = 501;
-      handler.respond(request, response, status, type);
-      break;
-    default:
-      status = 404;
-      handler.respond(request, response, status, type);
-      break;
+        break;
+      case '/badRequest':
+        if (params.valid) {
+          status = 200;
+        } else {
+          status = 400;
+        }
+        break;
+      case '/unauthorized':
+        if (params.loggedIn === 'yes') {
+          status = 200;
+        } else {
+          status = 401;
+        }
+        break;
+      case '/forbidden':
+        status = 403;
+        break;
+      case '/internal':
+        status = 500;
+        break;
+      case '/notImplemented':
+        status = 501;
+        break;
+      default:
+        status = 404;
+        break;
+    }
+    handler.respond(request, response, status, type);
   }
 };
 
 const onRequest = (request, response) => {
   const parsedURL = url.parse(request.url);
-  // console.log("Recieved a request.");
   handleGET(request, response, parsedURL);
 };
 
